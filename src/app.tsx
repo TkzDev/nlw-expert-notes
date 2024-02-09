61674ced6f3943397b2336b2369d206ecc35ab4e
TkzDev <98072572+TkzDev@users.noreply.github.com>
2024-02-09 15:30:18 -0300

    Revert "Finish nlw-expert-notes | RocketSeat"
    This reverts commit a144d6ed992f57d2029eb82907c4f3e504ffa75f.

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,32 @@
-import logo from "./assets/logo-nlw-expert.svg";
-import { NewNoteCard } from "./components/new-note-card";
-import { NoteCard } from "./components/note-card";
+import { NewNoteCard } from './assets/components/new-note-card'
+import { NoteCard } from './assets/components/note-card'
+import logo from './assets/logo-nlw-expert.svg'
+
 
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
-      <img src={logo} alt="NLW Expert" />
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={logo} alt='NLW Expert' />
 
-      <form className="w-full">
+      <form className='w-full'>
         <input
-          type="text"
-          placeholder="Busque em suas notas..."
-          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+          type='text' 
+          placeholder='Busque em suas notas...'
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
         />
       </form>
 
-      <div className="h-px bg-slate-700" />
+      <div className='h-px bg-slate-700' />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard 
+          note={{
+            date: new Date(),
+            content: 'O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where... Para configurar o plugin, é preciso instalar como abaixo',
+          }}
+        />
       </div>
     </div>
-  );
+  )
 }
